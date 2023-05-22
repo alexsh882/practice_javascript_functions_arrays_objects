c761b8edf58d8725b1b2584dabf0f8a56e44da7d
alexsh882 <alexsh882@gmail.com>
2023-05-22 14:13:45 -0300

    Se agregó la función cancularAreaRectangulo

--- functions.js
+++ functions.js
@@ -10,3 +10,16 @@ function esMayorEdad(edad) {
 console.log("Función esMayorEdad:");
 console.log(esMayorEdad(18));
 console.log(esMayorEdad(17));
+
+
+// Crea una función llamada calcularAreaRectangulo que tome dos parámetros: base y altura.
+// La función debe calcular y devolver el área del rectángulo utilizando la fórmula área = base *
+// altura.
+
+console.log("");
+console.log("Función calcularAreaRectangulo:");
+console.log(calcularAreaRectangulo(10, 20));
+
+function calcularAreaRectangulo(base, altura) {
+  return base * altura;
+}
