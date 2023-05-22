f1ed3ac833925ff0f51985d04cc623a7313ee607
alexsh882 <alexsh882@gmail.com>
2023-05-22 14:13:13 -0300

    Se agregó un index y el js la funcion esMayorEdad

---
+++ functions.js
@@ -0,0 +1,12 @@
+document.title = "Práctica Js"
+
+// Escribe una función llamada &quot;esMayorEdad&quot; que reciba una edad como parámetro y devuelva
+// true si la edad es mayor o igual a 18, y false en caso contrario.
+
+function esMayorEdad(edad) {
+  return edad >= 18;
+}
+
+console.log("Función esMayorEdad:");
+console.log(esMayorEdad(18));
+console.log(esMayorEdad(17));
