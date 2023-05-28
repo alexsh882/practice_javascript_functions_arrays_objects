e0a62de646381d83022b36ec7ae8e1446d2fdc16
Alejandro <alexsh882@gmail.com>
2023-05-28 20:52:01 -0300

    Se agregó la funcion esPrimo, para al ingresar un numero informe si es o no numero primo

--- all.js
+++ all.js
@@ -11,3 +11,26 @@ const queNumeroEs = (numero) => {
 
 queNumeroEs(numero);
 
+// 2)	Crear un programa que pida un numero al usuario y le devuelva un mensaje diciendo si el número es primo o no.
+// Aclaración: los números primos son aquellos que tienen exactamente 4 divisores.
+// 2 positivos y 2 negativos y que son mayores que 1.
+
+const numeroPrimo = prompt("Ingresa un número para saber si es primo");
+
+const esPrimo = (numeroPrimo) => {
+    
+    //Un número es primo solo si es divisible entre 1 y sí mismo.
+    //Por ejemplo, el número 4 no es primo porque es divisible entre 2.
+
+
+    if (numeroPrimo == 0 || numeroPrimo == 1 || numeroPrimo == 4) return "El número NO es PRIMO";
+
+    for (let i = 2; i < numeroPrimo / 2 ; i++) {
+        if (numeroPrimo % i == 0) return "El número NO es PRIMO";        
+    }
+    // Si no pasa lo anterior, el número es primo
+    return "El número es PRIMO";
+}
+
+alert(esPrimo(numeroPrimo));
+
