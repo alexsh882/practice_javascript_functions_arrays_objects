c82b036221df35f8ad301072c8484428f21c71d4
Alejandro <alexsh882@gmail.com>
2023-05-28 13:50:55 -0300

    Se agregó la funcion obtenerSuma

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let boton = document.querySelector('button');
+
+boton.innerText = 'Botón';
+
+
+// Crea una función llamada obtenerSuma 
+// que reciba un arreglo de números como parámetro y
+// devuelva la suma de todos los elementos del arreglo.
+
+let numbers = [32,6,10,11,15,16,18,13,5,9,21,29,36];
+
+
+const obtenerSuma = (array) => {
+    let sumatoria = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumatoria =  sumatoria + array[i];        
+    }
+
+    return sumatoria;
+
+}
+
+console.log(obtenerSuma(numbers));
